b257a9636b12fef4384ed262e80217be59c3e1c9
Jeremias Baez <jere.baez91@gmail.com>
2020-06-16 01:10:50 -0300

    Agregado TP6 (que tiene la funcionalidad del TP5 incluido)

--- TP6/tp6.py
+++ TP6/tp6.py
@@ -6,7 +6,100 @@
 #           Jeremias Baez Carballo
 #
 
-import sys
-import cv2 as cv
+import cv2
 import numpy as np
+import os
 
+path = os.path.dirname(os.path.realpath(__file__))
+print(path)
+
+mpress = False
+pInicial = (-1, -1)
+pFinal = (-1, -1)
+pSupport = (0, 0)
+
+def seleccion(event, x, y, flags, param):
+    global mpress, pInicial, pFinal, pSupport, img, original
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("Mouse button pressed")
+        mpress = True
+        pInicial = x, y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if mpress == True:
+            # print("Mouse button dragging")
+            if pSupport != (x, y):
+                pSupport = x, y
+                img = original.copy()
+                cv2.rectangle(img, pInicial, pSupport, (0, 0, 0), 1)
+                cv2.imshow("Imagen", img)
+    elif event == cv2.EVENT_LBUTTONUP:
+        print("Mouse button released")
+        mpress = False
+        pFinal = x, y
+        cv2.rectangle(img, pInicial, pFinal, (0, 0, 0), 1)
+        cv2.imshow("Imagen", img)
+
+def translacion(imagen, x, y):
+    height, width = imagen.shape[0], imagen.shape[1]
+    matrix = np.float32([[1, 0, x], [0, 1, y]])
+    shifted = cv2.warpAffine(imagen, matrix, (width, height))
+    return shifted
+
+def rotacion(imagen, angulo, centro=None, escala=1.0):
+    height, width = imagen.shape[0], imagen.shape[1]
+    if centro == None:
+        centro = height/2, width/2
+    matrix = cv2.getRotationMatrix2D(centro, angulo, escala)
+    rotated = cv2.warpAffine(imagen, matrix, (width, height))
+    return rotated
+
+try:
+    img = cv2.imread(path+"/img/carpincherto.jpg", cv2.IMREAD_COLOR)
+    original = img.copy()
+    cv2.namedWindow("Imagen")
+    cv2.setMouseCallback("Imagen", seleccion)
+    print("Seleccione un rectangulo en la imagen presionando y arrastrando el mouse.\
+         Presione g para guardar, r para volver a la imagen original, e para transformacion euclidiana\
+            , s para similaridad y q para salir\n")
+    xySize = img.shape
+    print("Size: ", xySize)        
+    while(True):
+        cv2.imshow("Imagen", img)
+        key = cv2.waitKey(0) & 0xFF
+        if key == ord("g"):
+            print("Guardando imagen como resultado.png\n")
+            result = img[pInicial[1]:pFinal[1], pInicial[0]:pFinal[0]]
+            cv2.imwrite(path+"/img/resultado.png", result)
+        elif key == ord("r"):
+            print("Restaurando imagen original\n")
+            img = original.copy()
+        elif key == ord("e"):
+            print("Se realizara una transformacion euclidiana de la selección, se guardara como\
+                 resultadoe.png\n")
+            angulo = float(input("Ingrese el angulo que desea rotar\n"))
+            translx = int(input("Ingrese la translacion en x\n"))
+            transly = int(input("Ingrese la translacion en y\n"))
+            result = img[pInicial[1]:pFinal[1], pInicial[0]:pFinal[0]]
+            result = translacion(result, translx, transly)
+            # print("translacion correcta")
+            result = rotacion(result, angulo)
+            cv2.imwrite(path+"/img/resultadoe.png", result)
+        elif key == ord("s"):
+            print("Se realizara una transformacion de similaridad de la selección, se guardara como\
+                 resultadosim.png\n")
+            translx = int(input("Ingrese la translacion en x\n"))
+            transly = int(input("Ingrese la translacion en y\n"))
+            angulo = float(input("Ingrese el angulo que desea rotar\n"))
+            escala = float(input("Ingrese la escala que quiere aplicar\n"))
+            result = img[pInicial[1]:pFinal[1], pInicial[0]:pFinal[0]]
+            result = translacion(result, translx, transly)
+            # print("translacion correcta")
+            result = rotacion(result, angulo, escala=escala)
+            cv2.imwrite(path+"/img/resultadosim.png", result)
+        elif key == ord("q"):
+            print("Saliendo del programa...\n")
+            break
+    cv2.destroyAllWindows()
+except Exception as e:
+    print("Excepcion ejecutando el programa: %s", e)
+    cv2.destroyAllWindows()
--- TPsOpcionales/practica5.py
+++ TPsOpcionales/practica5.py
@@ -3,7 +3,6 @@
 
 import cv2
 import numpy as np
-import pathlib
 import os
 
 path = os.path.dirname(os.path.realpath(__file__))
@@ -12,28 +11,32 @@ print(path)
 mpress = False
 ix, iy = 1, 1
 fx, fy = 1, 1
+supportx, supporty = 1, 1
 
 def seleccion(event, x, y, flags, param):
-    global mpress, mode, ix, iy, fx, fy
+    global mpress, mode, ix, iy, fx, fy, supportx, supporty, img, original
     if event == cv2.EVENT_LBUTTONDOWN:
         print("Mouse button pressed")
         mpress = True
         ix, iy = x, y
     elif event == cv2.EVENT_MOUSEMOVE:
         if mpress == True:
-            pass
             # print("Mouse button dragging")
-            # cv2.rectangle(img, (ix,iy), (x,y), (0, 0, 0), 2)
-            # cv2.imshow("Imagen", img)
+            if (supportx != x) | (supporty !=  y):
+                supportx, supporty = x, y
+                img = original.copy()
+                cv2.rectangle(img, (ix,iy), (supportx, supporty), (0, 0, 0), 1)
+                cv2.imshow("Imagen",img)
     elif event == cv2.EVENT_LBUTTONUP:
         print("Mouse button released")
         mpress = False
         fx, fy = x, y
-        cv2.rectangle(img, (ix,iy), (fx,fy), (0, 0, 0), 2)
+        cv2.rectangle(img, (ix,iy), (fx,fy), (0, 0, 0), 1)
         cv2.imshow("Imagen", img)
 
 try:
     img = cv2.imread(path+"/imgs/simpsons.jpeg", cv2.IMREAD_COLOR)
+    original = img.copy()
     cv2.namedWindow("Imagen")
     cv2.setMouseCallback("Imagen", seleccion)
     print("Seleccione un rectangulo en la imagen presionando y arrastrando el mouse.\
@@ -44,9 +47,12 @@ try:
         cv2.imshow("Imagen", img)
         key = cv2.waitKey(0) & 0xFF
         if key == ord("g"):
-            print("Guardando imagen como salidaSeleccion.png\n")
+            print("Guardando imagen como resultado.png\n")
+            result = img[iy:fy, ix:fx]
+            cv2.imwrite(path+"/imgs/resultado.png", result)
         elif key == ord("r"):
             print("Restaurando imagen original\n")
+            img = original.copy()
         elif key == ord("q"):
             print("Saliendo del programa...\n")
             break
